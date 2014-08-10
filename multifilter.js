c820ef2ebc8efa1ca1d99b6daf1a96c54b89c8c6
tatsushim <tatsu.shim@gmail.com>
2014-08-11 00:28:18 +0900

    Change the method of match data-col attribute for exact match

--- multifilter.js
+++ multifilter.js
@@ -6,8 +6,8 @@
       'method'    : 'thead' // This can be thead or class
     }, options);
 
-    jQuery.expr[":"].Contains = function(a, i, m) {
-      return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    jQuery.expr[":"].Equals = function(a, i, m) {
+      return (a.textContent || a.innerText || "").toUpperCase() === m[3].toUpperCase();
     };
 
     this.each(function() {
@@ -19,7 +19,7 @@
 
       if (settings.method === 'thead') {
         // Match the data-col attribute to the text in the thead
-        var col = container.find('th:Contains(' + $this.data('col') + ')');
+        var col = container.find('th:Equals(' + $this.data('col') + ')');
         var col_index = container.find($('thead th')).index(col);
       };
 
